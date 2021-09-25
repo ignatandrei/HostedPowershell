b1912bbfc7b120d2c40b7ec6e060b2bf88bc4df3
ignatandrei <ignatandrei@yahoo.com>
2021-09-25 23:30:39 +0300

    now executing 1 pwsh

--- src/HostedPowershell/HostedPowershell/BSPWSH.cs
+++ src/HostedPowershell/HostedPowershell/BSPWSH.cs
@@ -16,18 +16,19 @@ namespace HostedPowershell
         public int SleepSeconds { get; set; }
         public ExecuteV1Script[] scriptsV1 { get; set; }
     }
-    public class BSPWSH: BackgroundService
+    public class BSPWSH : BackgroundService
     {
         private readonly ILogger<BSPWSH> logger;
         private readonly IFileProvider fileProvider;
         private readonly IOptionsMonitor<BSPWSHOptions> optionsMonitor;
 
-        public BSPWSH(ILogger<BSPWSH> logger,IFileProvider fileProvider, IOptionsMonitor<BSPWSHOptions> optionsMonitor)
+        public BSPWSH(ILogger<BSPWSH> logger, IFileProvider fileProvider, IOptionsMonitor<BSPWSHOptions> optionsMonitor)
         {
             this.logger = logger;
             this.fileProvider = fileProvider;
             this.optionsMonitor = optionsMonitor;
-            
+            if (optionsMonitor == null)
+                throw new ArgumentException("please define scriptsv1");
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -35,19 +36,48 @@ namespace HostedPowershell
             while (!stoppingToken.IsCancellationRequested)
             {
                 logger.LogInformation($"BSPWSH is running at: {DateTime.Now}");
-                foreach (var script in optionsMonitor.CurrentValue.scriptsV1){
-                    using(var stream = fileProvider.GetFileInfo(script.ScriptName).CreateReadStream()){
-                        using(var reader = new StreamReader(stream)){
-                            var scriptText = await reader.ReadToEndAsync();
-                            script.scriptText=scriptText;
+                if (optionsMonitor.CurrentValue?.scriptsV1 == null)
+                {
+                    logger.LogInformation("please add scriptsv1");
+
+                }
+                else
+                {
+                    var scripts = optionsMonitor.CurrentValue.scriptsV1.Select(it=>it).ToArray();
+                    foreach (var script in scripts)
+                    {
+                        try{
+                        using (var stream = fileProvider.GetFileInfo(script.ScriptName).CreateReadStream())
+                        {
+                            using (var reader = new StreamReader(stream))
+                            {
+                                var scriptText = await reader.ReadToEndAsync();
+                                script.scriptText = scriptText;
+                            }
+                        }
+                        }
+                        catch (Exception ex){
+                            logger.LogError(ex, $"error reading script {script.ScriptName}");
                         }
                     }
-                }logger.LogInformation($"BSPWSH is running at: {DateTime.Now}");
-                var executor=new ExecutePWSH(logger);
-                var data= await executor.ExecuteParalelV1(optionsMonitor.CurrentValue.scriptsV1);
-                string result = data.Aggregate("", (current, item) => current + $"{item.ScriptName} : {item.Result?.Length}");
-                logger.LogInformation($"BSPWSH results : {result}");
-                await Task.Delay(optionsMonitor.CurrentValue.SleepSeconds * 1000, stoppingToken);
+                    var executor = new ExecutePWSH(logger);
+                    var dataToExecute= scripts.Where(x => x.scriptText != null).ToArray();
+                    if(dataToExecute.Length>0){
+                        var data = await executor.ExecuteParalelV1(dataToExecute);
+                        string result = data.Aggregate("", (current, item) => current + $"{item.ScriptName} : {item.Result?.Length}");
+                        logger.LogInformation($"BSPWSH results : {result}");
+                    }
+                    else{
+                        logger.LogInformation("no scripts found to execute");
+                    }
+
+                }
+                var delay = (optionsMonitor.CurrentValue?.SleepSeconds ?? 5);
+                if (delay == 0)
+                    delay = 5;
+
+                logger.LogInformation($"BSPWSH delay: {delay}");
+                await Task.Delay( delay * 1000, stoppingToken);
             }
         }
     }
